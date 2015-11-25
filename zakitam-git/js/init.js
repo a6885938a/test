585280b5b0de0cb34a2f09fa1fb0a8ee7a71cf2d
a6885938a <332178065@qq.com>
2015-11-25 15:05:46 +0800

    1

---
+++ zakitam-git/js/init.js
@@ -0,0 +1,17 @@
+/*
+	Aerial 1.0 by HTML5 UP
+	html5up.net | @n33co
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+*/
+
+// Skel.
+	skel.init({
+		reset: 'full',
+		breakpoints: {
+			'global': { range: '*', href: 'css/style.css', lockViewport: true, viewport: 'minimal-ui' },
+			'wide': { range: '-1680', href: 'css/style-wide.css' },
+			'normal': { range: '-1280', href: 'css/style-normal.css' },
+			'mobile': { range: '-640', href: 'css/style-mobile.css' },
+
+		}
+	});
