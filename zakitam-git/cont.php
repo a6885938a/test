724eeae9f653188364ba5df8027a5e1545171794
a6885938a <332178065@qq.com>
2015-12-16 23:40:35 +0800

    add

---
+++ zakitam-git/cont.php
@@ -0,0 +1,233 @@
+
+<div class=" row column" style=" width:100%;overflow:hidden;"> <img alt="" style="width:100%; max-width:100%; overflow:hidden;" src="images/bike.jpg" /> </div>
+
+<!--标题结束-->
+
+  <div class="layout import-collection visible-md visible-lg" style=" margin-top:20px;">
+    <div class="tit-name">套餐精选</div>
+    <div class="bd fd-clr" id="accordion">
+      <ul class="fd-clr">
+        <li class="t current"> <a  target="_blank" class="left">
+          <div class="triangle"> </div>
+          <div class="map"> <img class="background" src="images/b1.png" alt=""/> </div>
+          <img class="background" src="images/f1.jpg"  alt=""/> </a>
+          <div class="right">
+            <div class="top fd-clr">
+              <div class="icon">
+                <div class="ch">运动套装</div>
+                <div class="en">Sport</div>
+              </div>
+              <div class="paragraph">
+                <div class="text">集Gopro产品优点于一身，是我们推出的最小、最轻、最
+                  方便的摄像机，采用坚固耐用的防水设计，轻松一件操控，可拍摄1080P60视频和800万像素照片.</div>
+                <div class="keys">
+                  <p>套餐标配 </p>
+                  <ul>
+                    <li><a target="_blank" href="#"><span>HER04</span></a></li>
+                    <li><a target="_blank" href="#"><span>HER04</span></a></li>
+                    <li><a target="_blank" href="#"><span>HER04</span></a></li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+            <ul class="bottom">
+              <li > <a href="http://www.rmthsc.com/index.php/product-8834.html" target="_blank">
+                <div class="pic-box">
+                  <div class="pic"> <img width="120" height="170" src="images/p1.jpg" alt=""/> </div>
+                  <div class="pic-hover">
+                    <p class="title"></p>
+                    <p class="price"><span class="fd-cny">&yen;</span><span>168</span></p>
+                    <p class="icon"></p>
+                  </div>
+                </div>
+                <p>HER04 Session</p>
+                </a> </li>
+              <li > <a href="http://www.rmthsc.com/index.php/product-8835.html" target="_blank">
+                <div class="pic-box">
+                  <div class="pic"> <img width="120" height="170" src="images/p2.jpg" alt=""/> </div>
+                  <div class="pic-hover ">
+                    <p class="title"></p>
+                    <p class="price"><span class="fd-cny">&yen;</span><span>198</span></p>
+                    <p class="icon"></p>
+                  </div>
+                </div>
+                <p>三星64G C10卡</p>
+                </a> </li>
+              <li > <a href="http://www.rmthsc.com/index.php/product-8835.html" target="_blank">
+                <div class="pic-box">
+                  <div class="pic"> <img width="120" height="170" src="images/p3.jpg" alt=""/> </div>
+                  <div class="pic-hover ">
+                    <p class="title"></p>
+                    <p class="price"><span class="fd-cny">&yen;</span><span>198</span></p>
+                    <p class="icon"></p>
+                  </div>
+                </div>
+                <p>三星64G C10卡</p>
+                </a> </li>
+            </ul>
+          </div>
+        </li>
+        <li class="t"> <a  target="_blank" class="left">
+          <div class="triangle"> </div>
+          <div class="map"> <img class="background" src="images/b1.png" alt=""/> </div>
+          <img class="background" src="images/f1.jpg"  alt=""/> </a>
+          <div class="right">
+            <div class="top fd-clr">
+              <div class="icon">
+                <div class="ch">运动套装</div>
+                <div class="en">Sport</div>
+              </div>
+              <div class="paragraph">
+                <div class="text">集Gopro产品优点于一身，是我们推出的最小、最轻、最
+                  方便的摄像机，采用坚固耐用的防水设计，轻松一件操控，可拍摄1080P60视频和800万像素照片.</div>
+                <div class="keys">
+                  <p>套餐标配 </p>
+                  <ul>
+                    <li><a target="_blank" href="#"><span>HER04</span></a></li>
+                    <li><a target="_blank" href="#"><span>HER04</span></a></li>
+                    <li><a target="_blank" href="#"><span>HER04</span></a></li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+            <ul class="bottom">
+              <li > <a href="http://www.rmthsc.com/index.php/product-8834.html" target="_blank">
+                <div class="pic-box">
+                  <div class="pic"> <img width="120" height="170" src="images/p1.jpg" alt=""/> </div>
+                  <div class="pic-hover">
+                    <p class="title"></p>
+                    <p class="price"><span class="fd-cny">&yen;</span><span>168</span></p>
+                    <p class="icon"></p>
+                  </div>
+                </div>
+                <p>HER04 Session</p>
+                </a> </li>
+              <li > <a href="http://www.rmthsc.com/index.php/product-8835.html" target="_blank">
+                <div class="pic-box">
+                  <div class="pic"> <img width="120" height="170" src="images/p2.jpg" alt=""/> </div>
+                  <div class="pic-hover ">
+                    <p class="title"></p>
+                    <p class="price"><span class="fd-cny">&yen;</span><span>198</span></p>
+                    <p class="icon"></p>
+                  </div>
+                </div>
+                <p>三星64G C10卡</p>
+                </a> </li>
+              <li > <a href="http://www.rmthsc.com/index.php/product-8835.html" target="_blank">
+                <div class="pic-box">
+                  <div class="pic"> <img width="120" height="170" src="images/p3.jpg" alt=""/> </div>
+                  <div class="pic-hover ">
+                    <p class="title"></p>
+                    <p class="price"><span class="fd-cny">&yen;</span><span>198</span></p>
+                    <p class="icon"></p>
+                  </div>
+                </div>
+                <p>三星64G C10卡</p>
+                </a> </li>
+            </ul>
+          </div>
+        </li>
+        <li class="t"> <a  target="_blank" class="left">
+          <div class="triangle"> </div>
+          <div class="map"> <img class="background" src="images/b1.png" alt=""/> </div>
+          <img class="background" src="images/f1.jpg"  alt=""/> </a>
+          <div class="right">
+            <div class="top fd-clr">
+              <div class="icon">
+                <div class="ch">运动套装</div>
+                <div class="en">Sport</div>
+              </div>
+              <div class="paragraph">
+                <div class="text">集Gopro产品优点于一身，是我们推出的最小、最轻、最
+                  方便的摄像机，采用坚固耐用的防水设计，轻松一件操控，可拍摄1080P60视频和800万像素照片.</div>
+                <div class="keys">
+                  <p>套餐标配 </p>
+                  <ul>
+                    <li><a target="_blank" href="#"><span>HER04</span></a></li>
+                    <li><a target="_blank" href="#"><span>HER04</span></a></li>
+                    <li><a target="_blank" href="#"><span>HER04</span></a></li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+            <ul class="bottom">
+              <li > <a href="http://www.rmthsc.com/index.php/product-8834.html" target="_blank">
+                <div class="pic-box">
+                  <div class="pic"> <img width="120" height="170" src="images/p1.jpg" alt=""/> </div>
+                  <div class="pic-hover">
+                    <p class="title"></p>
+                    <p class="price"><span class="fd-cny">&yen;</span><span>168</span></p>
+                    <p class="icon"></p>
+                  </div>
+                </div>
+                <p>HER04 Session</p>
+                </a> </li>
+              <li > <a href="http://www.rmthsc.com/index.php/product-8835.html" target="_blank">
+                <div class="pic-box">
+                  <div class="pic"> <img width="120" height="170" src="images/p2.jpg" alt=""/> </div>
+                  <div class="pic-hover ">
+                    <p class="title"></p>
+                    <p class="price"><span class="fd-cny">&yen;</span><span>198</span></p>
+                    <p class="icon"></p>
+                  </div>
+                </div>
+                <p>三星64G C10卡</p>
+                </a> </li>
+              <li > <a href="http://www.rmthsc.com/index.php/product-8835.html" target="_blank">
+                <div class="pic-box">
+                  <div class="pic"> <img width="120" height="170" src="images/p3.jpg" alt=""/> </div>
+                  <div class="pic-hover ">
+                    <p class="title"></p>
+                    <p class="price"><span class="fd-cny">&yen;</span><span>198</span></p>
+                    <p class="icon"></p>
+                  </div>
+                </div>
+                <p>三星64G C10卡</p>
+                </a> </li>
+            </ul>
+          </div>
+        </li>
+      </ul>
+    </div>
+  </div>
+  <div  class="product">
+    <div class="container " style="margin-top:20px; ">
+      <div class="tit-name">产品分类</div>
+      <div class="col-md-12 column">
+        <div class="col-xs-6 col-sm-4 col-md-3 col-md-2 column"> <img src="images/g1.jpg"/> </div>
+        <div class="col-xs-6 col-sm-4 col-md-3 col-md-2 column"> <img src="images/g2.jpg"/> </div>
+        <div class="col-xs-6 col-sm-4 col-md-3 col-md-2  column"> <img src="images/g3.jpg"/> </div>
+        <div class="col-xs-6 col-sm-4 col-md-3 col-md-2  column"> <img src="images/g4.jpg"/> </div>
+        <div class="col-xs-6 col-sm-4 col-md-3 col-md-2  column"> <img src="images/g5.jpg"/> </div>
+        <div class="col-xs-6 col-sm-4 col-md-3 col-md-2  column"> <img src="images/g6.jpg"/> </div>
+      </div>
+    </div>
+  </div>
+  <div class="container h100" style="margin-top:20px;height:300px; overflow:hidden;">
+    <div class="row h100" >
+      <div class="content col-md-6 col-sm-6 col-xs-12 col-md-push-6 col-sm-push-6 text-center"  id="scene"  style="">
+        <div class="qr-codes hidden-sm hidden-xs"  style="position:relative; ">
+          <div class="note layer"   data-depth="0.20" > <img src=" images/body.png" /> </div>
+          <!--//note-->
+          <div class="container" style="position:absolute; top:200px; margin:0 auto; left:50px; max-width:500px;">
+            <div class="item col-md-6 col-sm-6 layer"   data-depth="0.80"  style=" margin:0 auto;"> <img src="images/w_img.jpg" alt="" />
+              <p>微信名片</p>
+            </div>
+            <!--//item-->
+            <div class="item col-md-6 col-sm-6 layer"   data-depth="0.80"  style=" margin:0 auto;"> <img src="images/t_img.png" alt="" />
+              <p>zaiktam淘宝店</p>
+            </div>
+            <!--//item--> 
+          </div>
+        </div>
+        <!--//qr-codes--> 
+      </div>
+      <!--//content-->
+      <div id="video-container" class=" h100 video-container col-md-6 col-sm-6 col-xs-12 col-md-pull-6 col-sm-pull-6 " >
+        <iframe width=100% height="100%" src="http://player.youku.com/embed/XMTM5MzQ2OTE0MA==" frameborder=0 allowfullscreen></iframe>
+      </div>
+      <!--//video--> 
+    </div>
+    <!--//row--> 
+  </div>
+  
---
+++ zakitam-git/footer.php
@@ -0,0 +1,23 @@
+  <div class="zt-f">
+    <div class="footer visible-md visible-lg">
+      <div class="footer-wrap">
+        <div class="col-sm-4 column" style="text-align:left;"> <a  href="#" style="line-height:70px;"><img class="logo" src="images/logo_w.png" height="38" style=" vertical-align:middle;" /></a> </div>
+        <div class="col-sm-4 column">
+          <ul style="line-height:70px;">
+            <li><img src="images/ico/weixin.png"/></li>
+            <li><img src="images/ico/weibo.png"/></li>
+            <li><img src="images/ico/youku.png"/></li>
+            <li><img src="images/ico/taobao.png"/></li>
+          </ul>
+        </div>
+        <div class="col-sm-4 column">
+          <p>Copyright @  Zaiktam Team 2015</p>
+        </div>
+      </div>
+    </div>
+    <div class="v-wrap visible-xs">
+      <div class="col-sm-12 column" style=" margin:0 auto;">
+        <p>Copyright @  Zaiktam Team 2015</p>
+      </div>
+    </div>
+  </div>
