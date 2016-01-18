cff77475bbe02c326b6c066f9673ef0d9aa82662
a6885938a <332178065@qq.com>
2016-01-18 15:09:00 +0800

    a

--- zakitam-git/header.php
+++ zakitam-git/header.php
@@ -13,7 +13,7 @@ var _hmt = _hmt || [];
  <div class="nav-wrap" >
     <div class="navbar-header">
       <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1"> <span class="sr-only">Toggle navigation</span> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button>
-      <a class="navbar-brand" style="text-align:center;" href="/index.php"><img src="/images/logo_w.png" height="38" style=" vertical-align:middle" /></a> </div>
+      <a class="navbar-brand visible-md visible-lg" style="text-align:center;" href="/index.php"><img src="/images/logo_w.png" height="38" style=" vertical-align:middle" /></a> </div>
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1" >
       <ul class="nav navbar-nav">
         <li class="active"> <a href="/index.php">首页</a> </li>
